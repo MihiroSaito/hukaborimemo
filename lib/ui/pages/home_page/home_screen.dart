024ff36bdafd51d342659c0f2bf6b1ce7b1e60a5
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2021-10-21 11:48:29 +0900

    HomePageにappbarを追加した

--- lib/ui/pages/home_page/home_screen.dart
+++ lib/ui/pages/home_page/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hukaborimemo/ui/pages/home_page/home_widget.dart';
 
 class HomeScreen extends HookWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,9 +11,7 @@ class HomeScreen extends HookWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-
-            )
+            homeAppBar()
           ],
         ),
       ),
--- lib/ui/pages/home_page/home_widget.dart
+++ lib/ui/pages/home_page/home_widget.dart
@@ -5,17 +5,43 @@ Widget homeAppBar() {
   return Container(
     height: 70,
     width: double.infinity,
+    padding: const EdgeInsets.only(left: 15, right: 15),
     child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Icon(
-          CupertinoIcons.gear,
-          size: 26,
-          //todo: ダークモード&ライトモードで管理できるように色をメソッドで管理する
+        Material(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+            onTap: () {
+              //todo: 設定ページへ
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                CupertinoIcons.gear_alt_fill,
+                size: 26,
+                //todo: ダークモード&ライトモードで管理できるように色をメソッドで管理する
+              ),
+            ),
+          ),
         ),
-        Icon(
-          CupertinoIcons.ellipsis,
-          size: 26,
-          //todo: テーマで管理できるように色をメソッドで管理する
+        Material(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+            onTap: () {
+              //todo: オプションを表示する
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                CupertinoIcons.ellipsis,
+                size: 26,
+                //todo: テーマで管理できるように色をメソッドで管理する
+              ),
+            ),
+          ),
         )
       ],
     ),
